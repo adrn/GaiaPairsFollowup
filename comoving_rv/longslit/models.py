b83e7f60b37098ad02b54cf54ba48f4a8d4fb8b4
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-03-24 11:35:14 -0400

    add voigt polynomial models

--- comoving_rv/longslit/models.py
+++ comoving_rv/longslit/models.py
@@ -1,8 +1,9 @@
 # Third-party
 import numpy as np
+from numpy.polynomial.polynomial import polyval
 from scipy.special import wofz
 
-__all__ = ['voigt']
+__all__ = ['voigt', 'voigt_constant', 'voigt_polynomial']
 
 def voigt(x, amp, x0, G_std, L_fwhm):
     """
@@ -28,3 +29,43 @@ def voigt(x, amp, x0, G_std, L_fwhm):
     z = (_x + 1j*L_fwhm/2.) / (np.sqrt(2.)*G_std)
 
     return amp * wofz(z).real / (np.sqrt(2.*np.pi)*G_std)
+
+def voigt_constant(x, amp, x_0, std_G, fwhm_L, C):
+    """
+    Voigt profile plus a constant background.
+
+    Parameters
+    ----------
+    x : numeric, array_like
+    amp : numeric
+        Amplitude of the profile (integral).
+    x0 : numeric
+        Centroid.
+    G_std : numeric
+        Standard of deviation of the Gaussian component.
+    L_fwhm : numeric
+        FWHM of the Lorentzian component.
+    C : numeric
+        Background
+    """
+    return voigt(x, amp, x_0, std_G, fwhm_L) + C
+
+def voigt_polynomial(x, amp, x_0, std_G, fwhm_L, bg_coef):
+    """
+    Voigt profile plus a constant background.
+
+    Parameters
+    ----------
+    x : numeric, array_like
+    amp : numeric
+        Amplitude of the profile (integral).
+    x0 : numeric
+        Centroid.
+    G_std : numeric
+        Standard of deviation of the Gaussian component.
+    L_fwhm : numeric
+        FWHM of the Lorentzian component.
+    bg_coef : iterable
+        List of polynomial coefficients.
+    """
+    return voigt(x, amp, x_0, std_G, fwhm_L) + polyval(x-x_0, np.atleast_1d(bg_coef))

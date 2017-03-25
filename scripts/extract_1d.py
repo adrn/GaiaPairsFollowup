9d2dcb0193a60b12a7f24ba3bb67f17f06d71f54
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-03-24 22:39:42 -0400

    todo

--- scripts/extract_1d.py
+++ scripts/extract_1d.py
@@ -22,6 +22,10 @@ disperion direction.
 Wavelength calibration and radial velocity corrections are handled in
 subsequent scripts.
 
+TODO:
+- add support for pixel masks to remove nearby sources / pick the correct object
+- need to figure out best way to specify the true source and the nearby source...
+
 """
 
 # Standard library

663fe3c903be0c1cc1f2757acdde6f3fb88cef13
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-03-29 17:31:46 -0400

    placeholder script

--- scripts/solve_velocity.py
+++ scripts/solve_velocity.py
@@ -1,123 +1,57 @@
 """
 
+TODO: do both relative and absolute velocity stuff in here
+
 Solve for the relative velocities between two spectra by fitting line profiles
 to the Halpha absorption line (in pixel space) and computing the relative
 velocity
-
-NOTES:
-- It looks like some sky spectra have [OI] 5577Å, others have [OI] 6300Å - we
-    may need to fit for both and check?
-
-TODO:
--
-
 """
 
 # Standard library
 from os import path
-import logging
 
 # Third-party
+from astropy.time import Time
 import astropy.coordinates as coord
 from astropy.constants import c
 from astropy.io import fits
-from astropy.time import Time
-from astropy.table import Table, Column
+from astropy.table import Table
 import astropy.units as u
 import matplotlib.pyplot as plt
 import numpy as np
 
 # Project
-from comoving_rv.longslit import SkippableImageFileCollection
-from comoving_rv.longslit import voigt_polynomial
+from comoving_rv.log import logger
+from comoving_rv.longslit import SkippableImageFileCollection, voigt_polynomial
 from comoving_rv.longslit.wavelength import fit_spec_line
+from comoving_rv.velocity import bary_vel_corr, kitt_peak
 
-logger = logging.getLogger('relative_velocity')
-formatter = logging.Formatter('%(levelname)s:%(name)s:  %(message)s')
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.propagate = False
+def solve_radial_velocity(wavelength, flux, flux_ivar, header, plot=False):
 
-def bary_vel_corr(time, skycoord, location=None):
-    """
-    Barycentric velocity correction.
+    _sort = wavelength.argsort()
+    wavelength = wavelength[_sort]
+    flux = flux[_sort]
+    flux_ivar = flux_ivar[_sort]
 
-    Uses the ephemeris set with  ``astropy.coordinates.solar_system_ephemeris.set``
-    for corrections. For more information see `~astropy.coordinates.solar_system_ephemeris`.
-
-    Parameters
-    ----------
-    time : `~astropy.time.Time`
-        The time of observation.
-    skycoord: `~astropy.coordinates.SkyCoord`
-        The sky location to calculate the correction for.
-    location: `~astropy.coordinates.EarthLocation`, optional
-        The location of the observatory to calculate the correction for.
-        If no location is given, the ``location`` attribute of the Time
-        object is used
-
-    Returns
-    -------
-    vel_corr : `~astropy.units.Quantity`
-        The velocity correction to convert to Barycentric velocities. Should be added to the original
-        velocity.
-    """
-
-    if location is None:
-        if time.location is None:
-            raise ValueError('An EarthLocation needs to be set or passed '
-                             'in to calculate bary- or heliocentric '
-                             'corrections')
-        location = time.location
-
-    # ensure sky location is ICRS compatible
-    if not skycoord.is_transformable_to(coord.ICRS):
-        raise ValueError("Given skycoord is not transformable to the ICRS")
-
-    # ICRS position and velocity of Earth's geocenter
-    ep, ev = coord.solar_system.get_body_barycentric_posvel('earth', time)
-
-    # GCRS position and velocity of observatory
-    op, ov = location.get_gcrs_posvel(time)
-
-    # ICRS and GCRS are axes-aligned. Can add the velocities
-    velocity = ev + ov # relies on PR5434 being merged
-
-    # get unit ICRS vector in direction of SkyCoord
-    sc_cartesian = skycoord.icrs.represent_as(coord.UnitSphericalRepresentation)\
-                                .represent_as(coord.CartesianRepresentation)
-    return sc_cartesian.dot(velocity).to(u.km/u.s) # similarly requires PR5434
-
-def solve_radial_velocity(filename, wavelength_coef, done_list=None, plot=False):
-    hdulist = fits.open(filename)
-
-    # read both hdu's
-    hdu0 = hdulist[0]
-    hdu1 = hdulist[1]
-    name = hdu0.header['OBJECT']
-    logger.debug("\tObject: {}".format(name))
-
-    if done_list is not None and name in done_list:
-        return
-
-    # extract just the middle part of the CCD (we only really care about Halpha)
-    tbl = hdu1.data[200:1600][::-1]
-
-    # compute wavelength array for the pixels
-    wvln = np.polynomial.polynomial.polyval(tbl['pix'], wavelength_coef)
+    # from: http://www.star.ucl.ac.uk/~msw/lines.html
+    Halpha = 6562.80 # air, STP
 
     # ==============================
     # Fit a Voigt profile to H-alpha
     # ==============================
 
-    # extract region of SOURCE spectrum around Halpha
-    i1 = np.argmin(np.abs(wvln - 6460))
-    i2 = np.argmin(np.abs(wvln - 6665))
+    # TODO: could do this for H-beta too
 
-    wave = wvln[i1:i2+1]
-    flux = tbl['source_flux'][i1:i2+1]
-    ivar = tbl['source_ivar'][i1:i2+1]
+    # extract region of source spectrum around Halpha
+    target_wave = Halpha
+
+    # select 30 angstroms to either side of Halpha
+    i1 = np.argmin(np.abs(wavelength - (target_wave-30)))
+    i2 = np.argmin(np.abs(wavelength - (target_wave+30))) + 1
+
+    wave = wavelength[i1:i2]
+    flux = flux[i1:i2]
+    ivar = flux_ivar[i1:i2]
     halpha_fit_p = fit_spec_line(wave, flux, ivar,
                                  n_bg_coef=2, target_x=6563., absorp_emiss=-1.)
 
@@ -126,80 +60,31 @@ def solve_radial_velocity(filename, wavelength_coef, done_list=None, plot=False)
         fit_flux = voigt_polynomial(_grid, **halpha_fit_p)
 
         plt.figure(figsize=(14,8))
-        plt.title("OBJECT: {}, EXPTIME: {}".format(hdu0.header['OBJECT'],
-                                                   hdu0.header['EXPTIME']))
+        plt.title("OBJECT: {}, EXPTIME: {}".format(header['OBJECT'],
+                                                   header['EXPTIME']))
 
         plt.plot(wave, flux, marker='', drawstyle='steps-mid', alpha=0.5)
         plt.errorbar(wave, flux, 1/np.sqrt(ivar), linestyle='none',
                      marker='', ecolor='#666666', alpha=0.75, zorder=-10)
         plt.plot(_grid, fit_flux, marker='', alpha=0.75)
+        plt.show()# TODO
 
-    # =========================================
-    # Fit a Voigt profile to [OI] 6300 and 5577
-    # =========================================
+    dHalpha = halpha_fit_p['x_0'] - target_wave
+    RV = dHalpha / Halpha * c
+    print("Object: {}".format(header['OBJECT']))
+    print("Radial velocity: ", RV.to(u.km/u.s))
 
-    # needed for Barycenter correction
-    # earth_loc = coord.EarthLocation.of_site('KPNO')
+    sc = coord.SkyCoord(ra=header['RA'], dec=header['DEC'],
+                        unit=(u.hourangle, u.degree))
+    time = header['JD']*u.day + header['EXPTIME']/2.*u.second
+    time = Time(time.to(u.day), format='jd', scale='utc')
+    v_bary = bary_vel_corr(time, sc, location=kitt_peak)
+    RV_corr = (RV + v_bary).to(u.km/u.s)
+    print("Bary. correction: ", v_bary.to(u.km/u.s))
+    print("Radial velocity (bary. corrected): ", RV_corr)
+    print()
 
-    for target_wave in [5577.3387, 6300.3]:
-        # extract region of SKY spectrum around line
-        i1 = np.argmin(np.abs(wvln - (target_wave-25)))
-        i2 = np.argmin(np.abs(wvln - (target_wave+25)))
-
-        wave = wvln[i1:i2+1]
-        flux = tbl['background_flux'][i1:i2+1]
-        ivar = tbl['background_ivar'][i1:i2+1]
-
-        OI_fit_p = fit_spec_line(wave, flux, ivar, std_G0=1.,
-                                 n_bg_coef=2, target_x=target_wave,
-                                 absorp_emiss=1.)
-
-        print('[OI] {:.2f}'.format(target_wave))
-        print('∆x_0: {:.3f}'.format(OI_fit_p['x_0'] - target_wave))
-        print('amp: {:.3e}'.format(OI_fit_p['amp']))
-
-        chi2 = np.sum((voigt_polynomial(wave, **OI_fit_p) - flux)**2 * ivar)
-        print('chi2: {}'.format(chi2))
-
-        if plot:
-            _grid = np.linspace(wave.min(), wave.max(), 512)
-            fit_flux = voigt_polynomial(_grid, **OI_fit_p)
-
-            plt.figure(figsize=(14,8))
-            plt.title("OBJECT: {}, EXPTIME: {}".format(hdu0.header['OBJECT'],
-                                                       hdu0.header['EXPTIME']))
-
-            plt.plot(wave, flux, marker='', drawstyle='steps-mid', alpha=0.5)
-            plt.errorbar(wave, flux, 1/np.sqrt(ivar), linestyle='none',
-                         marker='', ecolor='#666666', alpha=0.75, zorder=-10)
-            plt.plot(_grid, fit_flux, marker='', alpha=0.75)
-
-    # from: http://www.star.ucl.ac.uk/~msw/lines.html
-    # Halpha = 6562.80 # air, STP
-    # OI_5577 = 5577.3387 # air, STP
-    # OI_6300 = 6300.30 # air, STP
-
-    # dOI = OI_fit_p['x_0'] - OI_5577
-    # dHalpha = halpha_fit_p['x_0'] - Halpha
-    # dlambda = dHalpha - dOI
-
-    # RV = dlambda / Halpha * c
-    # print("Radial velocity: ", RV.to(u.km/u.s))
-
-    # sc = coord.SkyCoord(ra=hdu0.header['RA'], dec=hdu0.header['DEC'],
-    #                     unit=(u.hourangle, u.degree))
-    # time = hdu0.header['JD']*u.day + hdu0.header['EXPTIME']/2.*u.second
-    # time = Time(time.to(u.day), format='jd', scale='utc')
-    # v_bary = bary_vel_corr(time, sc, location=earth_loc)
-    # RV_corr = (RV + v_bary).to(u.km/u.s)
-    # print("Bary. correction: ", v_bary.to(u.km/u.s))
-    # print("Radial velocity (bary. corrected): ", RV_corr)
-    # print()
-
-    if plot:
-        plt.show()
-
-def main(proc_path, polynomial_order, overwrite=False):
+def main(proc_path, overwrite=False):
     """ """
 
     proc_path = path.realpath(path.expanduser(proc_path))
@@ -219,41 +104,31 @@ def main(proc_path, polynomial_order, overwrite=False):
     else:
         raise RuntimeError("how?!")
 
-    # read master_wavelength file
-    pix_wav = np.genfromtxt(path.join(proc_path, 'master_wavelength.csv'),
-                            delimiter=',', names=True)
+    # # TODO: keep track of RV measurements?!
+    # rv_file = path.join(proc_path, 'rvs.ecsv')
+    # if path.exists(rv_file):
+    #     rv_tbl = Table.read(rv_file, format='ecsv')
+    # else:
+    #     rv_tbl = None
 
-    # fit a polynomial to pixel vs. wavelength
-    coef = np.polynomial.polynomial.polyfit(pix_wav['pixel'], pix_wav['wavelength'],
-                                            deg=polynomial_order)
-    pred = np.polynomial.polynomial.polyval(pix_wav['pixel'], coef) # TODO: plot residuals?
-    if np.any((pred - pix_wav['wavelength']) > 0.1):
-        logger.warning("Wavelength residuals are large! Consider using a higher-order "
-                       "polynomial, or check your wavelength calibration file." +
-                       str(pred - pix_wav['wavelength']))
-
-    # file to keep track of RV measurements
-    rv_file = path.join(proc_path, 'rvs.ecsv')
-    if path.exists(rv_file):
-        rv_tbl = Table.read(rv_file, format='ecsv')
-    else:
-        rv_tbl = None
-
-    # ========================
-    # Compute wavelength grids
-    # ========================
+    # ==================================
+    # Solve for absolute radial velocity
+    # ==================================
 
     if data_file is not None: # filename passed - only operate on that
-        solve_radial_velocity(data_file, coef, done_list=None, plot=True)
+        file_list = [data_file]
 
     else: # a path was passed - operate on all 1D extracted files
         proc_ic = SkippableImageFileCollection(proc_path, glob_pattr='1d_proc_*')
         logger.info("{} 1D extracted spectra found".format(len(proc_ic.files)))
+        file_list = proc_ic.files_filtered(imagetyp='OBJECT')
+        file_list = [path.join(proc_ic.location, base_fname) for base_fname in file_list]
 
-        logger.info("Beginning wavelength calibration...")
-        for base_fname in proc_ic.files_filtered(imagetyp='OBJECT'):
-            fname = path.join(proc_ic.location, base_fname)
-            solve_radial_velocity(fname)
+    for fname in file_list:
+        header = fits.getheader(fname, 0)
+        tbl = fits.getdata(fname, 1)
+        solve_radial_velocity(tbl['wavelength'], tbl['source_flux'], tbl['source_ivar'],
+                              header, plot=False)
 
     # if rv_tbl is None:
     #     rv_tbl = Table()
@@ -281,12 +156,7 @@ if __name__ == "__main__":
 
     parser.add_argument('-p', '--path', dest='proc_path', required=True,
                         help='Path to a PROCESSED night or chunk of data to process. Or, '
-                             'path to a specific comp file.')
-
-    parser.add_argument('--polyorder', dest='polynomial_order', default=11, type=int,
-                        help='TODO')
-
-    # TODO: polynomial order
+                             'path to a specific file.')
 
     args = parser.parse_args()
 
